0d3d85c4eccc34d53a4d37ca0815718b40fb7436
郡 元気 <OIBC34018@ojs.ac.jp>
2023-05-08 12:10:20 +0900

    曜日表示

--- Chapter01/Product/Program.cs
+++ Chapter01/Product/Program.cs
@@ -20,15 +20,14 @@ namespace ProductSample {
 
             #region 演習1
             //DateTime date = new DateTime(2023, 5, 8);
-            //DateTime date = DateTime.Today;
+            DateTime date = DateTime.Today;
 
-            //Console.WriteLine("今日の日付：" + date.Year + "年" + date.Month + "月" + date.Day + "日");
+            Console.WriteLine("今日の日付：" + date.Year + "年" + date.Month + "月" + date.Day + "日");
 
-            ////10日後を求める
-            //DateTime daysAfter10 = date.AddDays(10);
-            //Console.WriteLine("10日後：" + daysAfter10.Year + "年" + daysAfter10.Month + "月" + daysAfter10.Day + "日");
-            //DateTime daysBefore10 = date.AddDays(-10);
-            //Console.WriteLine("10日前：" + daysBefore10.Year + "年" + daysBefore10.Month + "月" + daysBefore10.Day + "日");
+            //10日後を求める
+            Console.WriteLine("10日後：" + date.AddDays(10).Year + "年" + date.AddDays(10).Month + "月" + date.AddDays(10).Day + "日");
+            DateTime daysBefore10 = date.AddDays(-10);
+            Console.WriteLine("10日前：" + date.AddDays(-10).Year + "年" + date.AddDays(-10).Month + "月" + date.AddDays(-10).Day + "日");
             #endregion
 
             #region 演習2
@@ -44,6 +43,35 @@ namespace ProductSample {
             TimeSpan interval = DateTime.Today - birth;
 
             Console.WriteLine("あなたは生まれてから今日まで"+interval.Days+"日目です。");
+            #endregion
+
+            #region 誕生日の曜日
+            switch ((int)birth.DayOfWeek)
+            {
+                case 0:
+                    Console.WriteLine("日曜日");
+                    break;
+                case 1:
+                    Console.WriteLine("月曜日");
+                    break;
+                case 2:
+                    Console.WriteLine("火曜日");
+                    break;
+                case 3:
+                    Console.WriteLine("水曜日");
+                    break;
+                case 4:
+                    Console.WriteLine("木曜日");
+                    break;
+                case 5:
+                    Console.WriteLine("金曜日");
+                    break;
+                case 6:
+                    Console.WriteLine("土曜日");
+                    break;
+                default:
+                    break;
+            }
             #endregion
         }
     }
