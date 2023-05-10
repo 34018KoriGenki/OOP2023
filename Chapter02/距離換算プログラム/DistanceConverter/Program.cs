6a91c80ad44b892c9045a04cd40dc69d68eb24fc
郡 元気 <OIBC34018@ojs.ac.jp>
2023-05-10 11:25:19 +0900

    距離換算プログラム完成

--- Chapter02/距離換算プログラム/DistanceConverter/Program.cs
+++ Chapter02/距離換算プログラム/DistanceConverter/Program.cs
@@ -16,16 +16,16 @@ namespace DistanceConverter {
         }
 
         //フィートからメートルへの対応表を出力
-        static void PrintFeetToMeterList(int start, int end) {
-            for (int feet = start; feet <= end; feet++) {
+        static void PrintFeetToMeterList(int start, int stop) {
+            for (int feet = start; feet <= stop; feet++) {
                 double meter = FeetConverter.ToMeter(feet);
                 Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
             }
         }
 
         //メートルからフィートへの対応表を出力
-        static void PrintMeterToFeetList(int start, int end) {
-            for (int meter = start; meter <= end; meter++) {
+        static void PrintMeterToFeetList(int start, int stop) {
+            for (int meter = start; meter <= stop; meter++) {
                 double feet = FeetConverter.FromMeter(meter);
                 Console.WriteLine("{0} m = {1:0.0000} ft", meter, feet);
             }
