933b38779ee942ea4a677725568439f9675cb494
郡 元気 <OIBC34018@ojs.ac.jp>
2023-05-16 12:09:22 +0900

    songs配列、コンストラクタ追加

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -7,6 +7,14 @@ using System.Threading.Tasks;
 namespace Exercise01 {
     class Program {
         static void Main(string[] args) {
+
+            var songs = new Song[] {
+                new Song("Let it be", "The Beatles", 243),
+                new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
+                new Song("Close To You", "Carpenters", 276),
+                new Song("Honesty", "Billy Joel", 231),
+                new Song("I Will Always Love You", "Whitney Houston", 273),
+            };
         }
     }
 }
--- Chapter02/演習問題/Exercise01/Song.cs
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -6,5 +6,18 @@ using System.Threading.Tasks;
 
 namespace Exercise01 {
     class Song {
+        string title;
+        string artistName;
+        int length;
+
+        public string Title { get => title; set => title = value; }
+        public string ArtistName { get => artistName; set => artistName = value; }
+        public int Length { get => length; set => length = value; }
+
+        public Song(string title,string artistName,int length){
+            Title = title;
+            ArtistName = artistName;
+            Length = length;
+        }
     }
 }
