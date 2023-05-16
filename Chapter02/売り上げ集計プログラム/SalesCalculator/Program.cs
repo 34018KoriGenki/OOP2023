eac6e4512c2175f565d08f56fecbc0a1a9c9c906
郡 元気 <OIBC34018@ojs.ac.jp>
2023-05-16 10:21:24 +0900

    ReadSalesメソッドをSalesCounterクラスへ移動

--- Chapter02/売り上げ集計プログラム/SalesCalculator/Program.cs
+++ Chapter02/売り上げ集計プログラム/SalesCalculator/Program.cs
@@ -8,39 +8,11 @@ using System.Threading.Tasks;
 namespace SalesCalculator {
     class Program {
         static void Main(string[] args) {   //エントリポイント
-            SalesCounter sales = new SalesCounter(ReadSales(@"data\Sales.csv"));
-            Dictionary<string,int> amountPerStore = sales.GetPerStoreSales();
-            foreach (KeyValuePair<string,int> obj in amountPerStore) {
+            SalesCounter sales = new SalesCounter(SalesCounter.ReadSales(@"data\Sales.csv"));
+            Dictionary<string, int> amountPerStore = sales.GetPerStoreSales();
+            foreach (KeyValuePair<string, int> obj in amountPerStore) {
                 Console.WriteLine("{0}  \t{1:C}", obj.Key, obj.Value);
             }
-        }
-
-        //売り上げデータを読み込み、Saleオブジェクトのリストを返す
-        static List<Sale> ReadSales(string filePath) {  //スタブ
-            
-            //売り上げデータを格納する
-            List<Sale> sales = new List<Sale>();
-            
-            //ファイルからすべてのデータを読み込む
-            string[] lines = File.ReadAllLines(filePath);
-            
-            //すべての行から一行ずつ取り出す
-            foreach (string line in lines) {
-                
-                //区切りで項目別に分ける
-                string[] items = line.Split(',');
-                
-                //Saleインスタンスを生成
-                Sale sale = new Sale {
-                    ShopName = items[0],
-                    ProductCategory = items[1],
-                    Amount = int.Parse(items[2]),
-                };
-                
-                //Saleインスタンスをコレクションに追加
-                sales.Add(sale);
-            }
-            return sales;
         }
     }
 }
--- Chapter02/売り上げ集計プログラム/SalesCalculator/SalesCounter.cs
+++ Chapter02/売り上げ集計プログラム/SalesCalculator/SalesCounter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +26,34 @@ namespace SalesCalculator {
                     dict[sale.ShopName] = sale.Amount;  //店名が存在しない(新規格納)
             }
             return dict;
+        }
+
+        //売り上げデータを読み込み、Saleオブジェクトのリストを返す
+        public static List<Sale> ReadSales(string filePath) {  //スタブ
+
+            //売り上げデータを格納する
+            List<Sale> sales = new List<Sale>();
+
+            //ファイルからすべてのデータを読み込む
+            string[] lines = File.ReadAllLines(filePath);
+
+            //すべての行から一行ずつ取り出す
+            foreach (string line in lines) {
+
+                //区切りで項目別に分ける
+                string[] items = line.Split(',');
+
+                //Saleインスタンスを生成
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2]),
+                };
+
+                //Saleインスタンスをコレクションに追加
+                sales.Add(sale);
+            }
+            return sales;
         }
     }
 }
