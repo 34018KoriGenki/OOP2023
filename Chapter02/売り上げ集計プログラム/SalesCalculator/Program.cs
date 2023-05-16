cb8fa59bd2ecb68881804cec48979e35e72a4873
郡 元気 <OIBC34018@ojs.ac.jp>
2023-05-16 11:33:29 +0900

    各コレクションにインターフェースの導入

--- Chapter02/売り上げ集計プログラム/SalesCalculator/Program.cs
+++ Chapter02/売り上げ集計プログラム/SalesCalculator/Program.cs
@@ -9,7 +9,7 @@ namespace SalesCalculator {
     class Program {
         static void Main(string[] args) {   //エントリポイント
             SalesCounter sales = new SalesCounter(@"data\Sales.csv");
-            Dictionary<string, int> amountPerStore = sales.GetPerStoreSales();
+            IDictionary<string, int> amountPerStore = sales.GetPerStoreSales();
             foreach (KeyValuePair<string, int> obj in amountPerStore) {
                 Console.WriteLine("{0}  \t{1:C}", obj.Key, obj.Value);
             }
--- Chapter02/売り上げ集計プログラム/SalesCalculator/SalesCounter.cs
+++ Chapter02/売り上げ集計プログラム/SalesCalculator/SalesCounter.cs
@@ -9,7 +9,7 @@ namespace SalesCalculator {
 
     public class SalesCounter {
 
-        private List<Sale> _sales;
+        private IEnumerable<Sale> _sales;
 
         //コンストラクタ
         public SalesCounter(string filePath) {
@@ -17,8 +17,8 @@ namespace SalesCalculator {
         }
 
         //店舗別売り上げを求める
-        public Dictionary<string,int> GetPerStoreSales() {
-            Dictionary<string,int> dict = new Dictionary<string, int>();
+        public IDictionary<string,int> GetPerStoreSales() {
+            SortedDictionary<string,int> dict = new SortedDictionary<string, int>();
             foreach (Sale sale in _sales) {
                 if (dict.ContainsKey(sale.ShopName))
                     dict[sale.ShopName] += sale.Amount; //店名が既に存在する(売り上げ加算)
@@ -29,7 +29,7 @@ namespace SalesCalculator {
         }
 
         //売り上げデータを読み込み、Saleオブジェクトのリストを返す
-        private static List<Sale> ReadSales(string filePath) {  //スタブ
+        private static IEnumerable<Sale> ReadSales(string filePath) {  //スタブ
 
             //売り上げデータを格納する
             List<Sale> sales = new List<Sale>();
