43954fe01b2abf630f89e92408d178b4d8b9cf1a
郡 元気 <OIBC34018@ojs.ac.jp>
2023-05-17 10:56:54 +0900

    プロパティの変更

--- Chapter02/演習問題/Exercise01/Song.cs
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -6,19 +6,16 @@ using System.Threading.Tasks;
 
 namespace Exercise01 {
     class Song {
-        string title;
-        string artistName;
-        int length;
 
-        public string Title { get => title; set => title = value; }
-        public string ArtistName { get => artistName; set => artistName = value; }
-        public int Length { get => length; set => length = value; }
+        public string Title { get; set; }
+        public string ArtistName { get; set; }
+        public int Length { get; set; }
 
-        public Song(string title,string artistName,int length){
+        public Song(string title, string artistName, int length) {
             Title = title;
             ArtistName = artistName;
             Length = length;
-            
+
         }
     }
 }
