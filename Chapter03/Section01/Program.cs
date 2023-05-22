6bfc099a3cc60fcc6a7670a74f056a607245f35b
郡 元気 <OIBC34018@ojs.ac.jp>
2023-05-22 11:24:30 +0900

    正解ver

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -13,8 +13,8 @@ namespace Section01 {
         public static int Count(int num) {
             var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4,};
             int count = 0;
-            foreach (var item in numbers) {
-                if (item == num) {
+            foreach (var n in numbers) {
+                if (n == num) {
                     count++;
                 }
             }
