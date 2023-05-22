cbd31ce357bce838dd9559ba87e9980d1819efa3
郡 元気 <OIBC34018@ojs.ac.jp>
2023-05-22 10:13:55 +0900

    売上集計追加

--- Chapter02/演習問題/Exercise03/Exercise03/Program.cs
+++ Chapter02/演習問題/Exercise03/Exercise03/Program.cs
@@ -8,14 +8,27 @@ namespace Exercise03 {
     class Program {
         static void Main(string[] args) {   //エントリポイント
             var sales = new SalesCounter(@"data\Sales.csv");
-            var amountPerStore = sales.GetPerStoreSales();
-            foreach (var obj in amountPerStore) {
-                Console.WriteLine("{0}  \t{1:C}", obj.Key, obj.Value);
-            }
+            Console.Write("＊＊売上集計＊＊\n1：店舗別売り上げ\n2：商品カテゴリ別売り上げ\n>");
+            int val = int.Parse(Console.ReadLine());
 
+            if (val == 1) {
+                AmountPerStore(sales);
+            } else if(val == 2) {
+                AmountPerCategory(sales);
+            } 
+        }
+
+        private static void AmountPerCategory(SalesCounter sales) {
             var amountPerCategory = sales.GetPerCategorySales();
             foreach (var obj in amountPerCategory) {
                 Console.WriteLine("{0}\t{1:C}", obj.Key, obj.Value);
+            }
+        }
+
+        private static void AmountPerStore(SalesCounter sales) {
+            var amountPerStore = sales.GetPerStoreSales();
+            foreach (var obj in amountPerStore) {
+                Console.WriteLine("{0}  \t{1:C}", obj.Key, obj.Value);
             }
         }
     }
