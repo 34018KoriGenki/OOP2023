51ddf4710eb10d2338468e060c4c2dba5144a584
郡 元気 <OIBC34018@ojs.ac.jp>
2023-05-22 10:40:03 +0900

    完成版

--- Chapter02/演習問題/Exercise03/Exercise03/Program.cs
+++ Chapter02/演習問題/Exercise03/Exercise03/Program.cs
@@ -7,29 +7,31 @@ using System.Threading.Tasks;
 namespace Exercise03 {
     class Program {
         static void Main(string[] args) {   //エントリポイント
+            IDictionary<string, int> amounts = null;
             var sales = new SalesCounter(@"data\Sales.csv");
             Console.Write("＊＊売上集計＊＊\n1：店舗別売り上げ\n2：商品カテゴリ別売り上げ\n>");
             int val = int.Parse(Console.ReadLine());
-
+            
             if (val == 1) {
-                AmountPerStore(sales);
-            } else if(val == 2) {
-                AmountPerCategory(sales);
-            } 
+                amounts = AmountPerStore(sales);
+            } else {
+                amounts = AmountPerCategory(sales);
+            }
+            PrintAmount(amounts);
         }
 
-        private static void AmountPerCategory(SalesCounter sales) {
-            var amountPerCategory = sales.GetPerCategorySales();
-            foreach (var obj in amountPerCategory) {
-                Console.WriteLine("{0}\t{1:C}", obj.Key, obj.Value);
+        private static void PrintAmount(IDictionary<string,int> amounts) {
+            foreach (var obj in amounts) {
+                Console.WriteLine("{0}   \t{1:C}", obj.Key, obj.Value);
             }
         }
 
-        private static void AmountPerStore(SalesCounter sales) {
-            var amountPerStore = sales.GetPerStoreSales();
-            foreach (var obj in amountPerStore) {
-                Console.WriteLine("{0}  \t{1:C}", obj.Key, obj.Value);
-            }
+        private static IDictionary<string, int> AmountPerCategory(SalesCounter sales) {
+            return sales.GetPerCategorySales();
+        }
+
+        private static IDictionary<string, int> AmountPerStore(SalesCounter sales) {
+            return sales.GetPerStoreSales();
         }
     }
 }
