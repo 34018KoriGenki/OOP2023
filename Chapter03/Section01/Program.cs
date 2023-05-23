5699c91ea9e605746d0b7483edf82674ea16147e
郡 元気 <OIBC34018@ojs.ac.jp>
2023-05-23 10:30:40 +0900

    P87まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -10,8 +10,12 @@ namespace Section01 {
         static void Main(string[] args) {
             var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4, };
 
-            int count = numbers.Count(n => n % 2 == 0);        //拡張メソッド,ラムダ演算式(ラムダ式)
-            Console.WriteLine(count);
+            //5の倍数
+            //int count = numbers.Count(n => n % 5 == 0);        //拡張メソッド,ラムダ演算式(ラムダ式)
+
+            //合計値
+            var sum = numbers.Where(n => n % 2 == 0).Average();
+            Console.WriteLine(sum);
         }
     }
 }
