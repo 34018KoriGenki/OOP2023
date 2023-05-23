7ccc34562eb9a8ea1ecade4abbf864fb54340a8f
郡 元気 <OIBC34018@ojs.ac.jp>
2023-05-23 10:51:11 +0900

    Section03追加

---
+++ Chapter03/Section03/Section03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section03 {
+    class Program {
+        static void Main(string[] args) {
+            var list = new List<string> {
+               "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+            var exists = list.Exists(s => s[0] == 'A');
+            Console.WriteLine(exists);
+
+        }
+    }
+}
