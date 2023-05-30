e215de71984675927ac557381d1c9091762c2448
郡 元気 <OIBC34018@ojs.ac.jp>
2023-05-30 11:55:58 +0900

    演習4-3(正解ver)

--- Chapter04/演習問題/Exercise01/YearMonth.cs
+++ Chapter04/演習問題/Exercise01/YearMonth.cs
@@ -10,20 +10,26 @@ namespace Exercise01 {
         public int Month { get; private set; }
         public bool Is21Century => 2001 <= Year && Year <= 2100;
 
-        public YearMonth(int year,int month) {
+        public YearMonth(int year, int month) {
             Year = year;
             Month = month;
         }
 
         public YearMonth AddOneMonth() {
-            int addYear = Month < 12 ? Year : Year + 1;
-            int addMonth = Month < 12 ? Month + 1 : 1;
-            return new YearMonth(addYear,addMonth);
+            if (Month == 12) {
+                return new YearMonth(Year + 1, 1);
+            } else {
+                return new YearMonth(Year, Month + 1);
+            }
+
+            //int addYear = Month < 12 ? Year : Year + 1;
+            //int addMonth = Month < 12 ? Month + 1 : 1;
+            //return new YearMonth(addYear,addMonth);
         }
 
         override
         public string ToString() {
-            return Year+"年"+Month+"月";
+            return Year + "年" + Month + "月";
         }
     }
 }
