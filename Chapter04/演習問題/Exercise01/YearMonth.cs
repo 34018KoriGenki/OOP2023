cf255594d1273580f8ec69aa5c072f9a67d78d45
郡 元気 <OIBC34018@ojs.ac.jp>
2023-05-30 11:21:48 +0900

    演習4-2,演習4-3

--- Chapter04/演習問題/Exercise01/YearMonth.cs
+++ Chapter04/演習問題/Exercise01/YearMonth.cs
@@ -5,13 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Exercise01 {
-    class YearMonth {
+    public class YearMonth {
         public int Year { get; private set; }
         public int Month { get; private set; }
+        public bool Is21Century => 2001 <= Year && Year <= 2100;
 
         public YearMonth(int year,int month) {
             Year = year;
             Month = month;
+        }
+
+        public YearMonth AddOneMonth() {
+            return new YearMonth(Year, Month < 12 ? Month+1 : 1);
         }
     }
 }
--- Chapter04/演習問題/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Exercise01;
 
 namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
+            var ym = new YearMonth(2023, 12);
+            var c21 = ym.Is21Century;
+            Console.WriteLine(c21);
+            var ym2 = ym.AddOneMonth();
+            Console.WriteLine(ym2.Month);
         }
     }
 }
