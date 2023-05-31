cbf32d25a619b1de805457010bd61fbae3b2f13d
郡 元気 <OIBC34018@ojs.ac.jp>
2023-05-31 11:41:53 +0900

    演習5-1完成

---
+++ Chapter05/演習問題/Exercise01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("1つ目の文字列：");
+            var str1 = Console.ReadLine();
+            Console.Write("2つ目の文字列：");
+            var str2 = Console.ReadLine();
+
+            if (string.Compare(str1, str2, ignoreCase: true) == 0) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+        }
+    }
+}
