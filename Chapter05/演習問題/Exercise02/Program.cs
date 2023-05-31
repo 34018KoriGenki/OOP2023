f2bce169b1ced62ba9dc17ff92c9a55bd140ee52
郡 元気 <OIBC34018@ojs.ac.jp>
2023-05-31 12:00:49 +0900

    演習5-2完成

---
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("数字：");
+            var str = Console.ReadLine();
+            if (int.TryParse(str,out var result)) {
+                Console.WriteLine ($"{result:#,0}") ;
+            }
+        }
+    }
+}
