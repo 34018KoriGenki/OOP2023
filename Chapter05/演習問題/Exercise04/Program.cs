636a3c42193254793e397fd7a76f5a649ab72c87
郡 元気 <OIBC34018@ojs.ac.jp>
2023-06-05 12:01:41 +0900

    演習問題5_4

---
+++ Chapter05/演習問題/Exercise04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise04 {
+    class Program {
+        static void Main(string[] args) {
+            var baseStr = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
+            var infoStr = new[] {"作家  ：","代表作：","誕生年："};
+            var words = baseStr.Split(';');
+            for (int i = 0;i < words.Length;i++) {
+                Console.WriteLine(infoStr[i]+words[i].Remove(0,words[i].IndexOf('=')+1));
+            }
+        }
+    }
+}
