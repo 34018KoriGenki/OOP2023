1737e3db03eb2fb2ba598b7e0c6292f65e9e4be2
郡 元気 <OIBC34018@ojs.ac.jp>
2023-06-05 09:36:59 +0900

    演習問題5_3_2

--- Chapter05/演習問題/Exercise03/Program.cs
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -31,7 +31,8 @@ namespace Exercise03 {
         }
 
         private static void Exercise3_2(string text) {
-            
+            var str = text.Replace("big","small");
+            Console.WriteLine(str);
         }
 
         private static void Exercise3_3(string text) {
