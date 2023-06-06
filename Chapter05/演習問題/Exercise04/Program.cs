2e03e188c7693892f3f48e2b8c7d2ef47827065f
郡 元気 <OIBC34018@ojs.ac.jp>
2023-06-06 10:18:42 +0900

    演習問題5-4配列対応版

--- Chapter05/演習問題/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,11 +8,40 @@ using System.Threading.Tasks;
 namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
+            Stopwatch sw = new Stopwatch();
+            sw.Start(); //タイマースタート
+#if NonArray
             var baseStr = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
-            var infoStr = new[] { "作家  ：", "代表作：", "誕生年：" };
+            ReceptionInfo(baseStr);
+#elif StringArray
+            string[] baseLines = { "Novelist=谷崎潤一郎a;BestWork=春琴抄;Born=1886",
+                                "Novelist=谷崎潤一郎b;BestWork=春琴抄;Born=1886",
+                                "Novelist=谷崎潤一郎c;BestWork=春琴抄;Born=1886",
+                                "Novelist=谷崎潤一郎d;BestWork=春琴抄;Born=1886",
+                                "Novelist=谷崎潤一郎e;BestWork=春琴抄;Born=1886",
+                                "Novelist=谷崎潤一郎f;BestWork=春琴抄;Born=1886",
+                                "Novelist=谷崎潤一郎g;BestWork=春琴抄;Born=1886",
+            };
+            ReceptionInfo(baseLines);
+#endif
+            Console.WriteLine("実行時間：{0}s",sw.Elapsed.ToString(@"ss\.ffffff")); //時間表示
+        }
+        
+        private static void ReceptionInfo(string[] baseLines) {
+            foreach (var baseStr in baseLines) {
+                ReceptionInfo(baseStr);
+            }
+        }
+
+        private static void ReceptionInfo(string baseStr) {
             var words = baseStr.Split(';');
+            Print(words);
+        }
+
+        private static void Print(string[] words) {
+            string[] infoStr = { "作家  ：", "代表作：", "誕生年：" };
             for (int i = 0;i < words.Length;i++) {
-                Console.WriteLine(infoStr[i % 3] + words[i].Remove(0, words[i].IndexOf('=') + 1));
+                Console.WriteLine(infoStr[i] + words[i].Remove(0, words[i].IndexOf('=') + 1));
             }
         }
     }
