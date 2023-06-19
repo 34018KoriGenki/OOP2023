2a2c8db05058eb5e1861f71a58a8aa6ad1486659
郡 元気 <OIBC34018@ojs.ac.jp>
2023-06-19 15:55:00 +0900

    設問２_提出

--- TestProject/Test02/Program.cs
+++ TestProject/Test02/Program.cs
@@ -37,7 +37,7 @@ namespace Test02 {
         //　　　　出力結果【618】
         private static void Exercise01(List<int> numbers) {
 
-
+            Console.WriteLine(numbers.Sum());
 
         }
 
@@ -45,14 +45,16 @@ namespace Test02 {
         //　　　　出力結果【94】
         private static void Exercise02(List<int> numbers) {
 
-
+            Console.WriteLine(numbers.Where(n => n % 2 == 0).Max());
 
         }
         //問題３　昇順に並べて表示（遅延実行とする）
         //　　　　出力結果【12 14 17 20 31 35 40 48 53 76 87 91 94】
         private static void Exercise03(List<int> numbers) {
 
-
+            foreach (var item in numbers.OrderBy(n => n)) {
+                Console.Write(item + " ");
+            }
 
         }
 
@@ -60,7 +62,9 @@ namespace Test02 {
         //　　　　出力結果【12 14 20 40 35 31 17 48】
         private static void Exercise04(List<int> numbers) {
 
-
+            foreach (var item in numbers.Where(n => 10 <= n && n <= 50)) {
+                Console.Write(item + " ");
+            }
 
         }
     }
