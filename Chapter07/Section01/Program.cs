4391758d9c1d6fe3144ec89afc03e16a44e7849b
郡 元気 <OIBC34018@ojs.ac.jp>
2023-06-22 11:57:49 +0900

    県庁所在地の登録

--- Chapter07/Section01/Program.cs
+++ Chapter07/Section01/Program.cs
@@ -7,21 +7,23 @@ using System.Threading.Tasks;
 namespace Section01 {
     class Program {
         static void Main(string[] args) {
-            var flowerDict = new Dictionary<string, int>() {
-                ["sunflower"] = 400,
-                ["pansy"] = 300,
-                ["tulip"] = 350,
-                ["rose"] = 500,
-                ["dahlia"] = 450,
-            };
+            Console.WriteLine("県庁所在地の登録");
+            var pref = new Dictionary<string, string>();
+            var str = "";
+            do {
+                Console.Write("県名：");
+                str = Console.ReadLine();
+                if (str == "999") break;
+                Console.Write("所在地：");
+                pref[str] = Console.ReadLine();
+            } while (true);
 
-            //morning glory(あさがお) 【250円】を追加
-
-            flowerDict["morning glory"] = 250;
-
-            Console.WriteLine("ひまわりの価格は{0}円です。",flowerDict["sunflower"]);
-            Console.WriteLine("チューリップの価格は{0}円です。",flowerDict["tulip"]);
-            Console.WriteLine("あさがおの価格は{0}円です。", flowerDict["morning glory"]);
+            Console.Write("県名を入力：");
+            try {
+                Console.WriteLine($"{pref[Console.ReadLine()]}です。");
+            } catch (Exception) {
+                Console.WriteLine("データが存在しません。");
+            }
         }
     }
 }
