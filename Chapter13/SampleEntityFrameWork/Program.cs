d0fa9934dc758b15e1642eabae242bb6d9f58899
郡 元気 <OIBC34018@ojs.ac.jp>
2023-09-14 11:36:27 +0900

    演習問題13完了

--- Chapter13/SampleEntityFrameWork/Program.cs
+++ Chapter13/SampleEntityFrameWork/Program.cs
@@ -38,12 +38,107 @@ namespace SampleEntityFrameWork {
             //    Console.WriteLine(count);
             //}
 
-
+            //Console.WriteLine("# 1.1");
+            //Exercise1_1();
 
             Console.WriteLine();
+            Console.WriteLine("# 1.2");
+            Exercise1_2();
+
+            Console.WriteLine();
+            Console.WriteLine("# 1.3");
+            Exercise1_3();
+
+            Console.WriteLine();
+            Console.WriteLine("# 1.4");
+            Exercise1_4();
+
+            Console.WriteLine();
+            Console.WriteLine("# 1.5");
+            Exercise1_5();
+
             Console.ReadLine();
 
         }
+
+        private static void Exercise1_1() {
+            using (var db = new BooksDbContext()) {
+                var author1 = new Author {
+                    Birthday = new DateTime(1888, 12, 26),
+                    Gender = "M",
+                    Name = "菊池寛",
+                };
+                db.Authors.Add(author1);
+                var author2 = new Author {
+                    Birthday = new DateTime(1899, 6, 14),
+                    Gender = "M",
+                    Name = "川端康成",
+                };
+                db.Authors.Add(author2);
+                db.SaveChanges();
+
+                var authorA = db.Authors.Single(a => a.Name == "夏目漱石");
+                var book1 = new Book {
+                    Title = "こころ",
+                    PublishedYear = 1991,
+                    Author = authorA,
+                };
+                db.Books.Add(book1);
+                var authorB = db.Authors.Single(a => a.Name == "川端康成");
+                var book2 = new Book {
+                    Title = "伊豆の踊子",
+                    PublishedYear = 2003,
+                    Author = authorB,
+                };
+                db.Books.Add(book2);
+                var authorC = db.Authors.Single(a => a.Name == "菊池寛");
+                var book3 = new Book {
+                    Title = "真珠夫人",
+                    PublishedYear = 2002,
+                    Author = authorC,
+                };
+                db.Books.Add(book3);
+                var authorD = db.Authors.Single(a => a.Name == "宮沢賢治");
+                var book4 = new Book {
+                    Title = "注文の多い料理店",
+                    PublishedYear = 2000,
+                    Author = authorD,
+                };
+                db.Books.Add(book4);
+                db.SaveChanges();
+            };
+
+        }
+
+        private static void Exercise1_2() {
+            DisplayAllBooks();
+        }
+
+        private static void Exercise1_3() {
+            var maxTitle = GetBooks().Max(b => b.Title.Length);
+            foreach (var book in GetBooks().Where(b => b.Title.Length == maxTitle)) {
+                Console.WriteLine(book.Title);
+            }
+        }
+
+        private static void Exercise1_4() {
+            foreach (var book in GetBooks().OrderBy(b => b.PublishedYear).ThenBy(b => b.Author.Name).Take(3)) {
+                Console.WriteLine($"{book.Title} {book.Author.Name}");
+            }
+        }
+
+        private static void Exercise1_5() {
+            var Groups = GetBooks().GroupBy(b => b.Author)
+                .OrderByDescending(b => b.Key.Birthday).ThenBy(b => b.Key.Name);
+            foreach (var bookGroup in Groups) {
+                Console.WriteLine($"{bookGroup.Key.Name}");
+                foreach (var book in bookGroup) {
+                    Console.WriteLine($"{book.Title} {book.PublishedYear}");
+                }
+                Console.WriteLine();
+            }
+        }
+
         // List 13-5
         static void InsertBooks() {
             using (var db = new BooksDbContext()) {
@@ -73,21 +168,29 @@ namespace SampleEntityFrameWork {
         }
 
         //List 13-7
+        //static IEnumerable<Book> GetBooks() {
+        //    using (var db = new BooksDbContext()) {
+        //        return db.Books.Where(book => book.PublishedYear > 1900)
+        //                        .Include(nameof(Author))
+        //                        .ToList();
+        //    }
+        //}
         static IEnumerable<Book> GetBooks() {
             using (var db = new BooksDbContext()) {
-                return db.Books.Where(book => book.PublishedYear > 1900)
-                                .Include(nameof(Author))
-                                .ToList();
+                return db.Books.Include(nameof(Author)).ToList();
             }
         }
 
         //List 13-8
+        //static void DisplayAllBooks() {
+        //    var books = GetBooks();
+        //    foreach (var book in books) {
+        //        Console.WriteLine($"{book.Title}{book.PublishedYear}");
+        //    }
+        //    Console.ReadLine();     //コンソールの画面をすぐ消さないためにキー入力待ちにする
+        //}
         static void DisplayAllBooks() {
-            var books = GetBooks();
-            foreach (var book in books) {
-                Console.WriteLine($"{book.Title}{book.PublishedYear}");
-            }
-            Console.ReadLine();     //コンソールの画面をすぐ消さないためにキー入力待ちにする
+            ((List<Book>)GetBooks()).ForEach(b => Console.WriteLine($"{ b.Title} { b.PublishedYear} {b.Author.Name}"));
         }
 
         //List 13-9
