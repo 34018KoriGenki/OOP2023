97b1cc67ee9c02834bdb3cbc082461d861fc55ff
郡 元気 <OIBC34018@ojs.ac.jp>
2023-10-17 10:21:34 +0900

    17章①継承を使ったポリモーフィズム

---
+++ Chapter17/Section01/Greeting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class GreetingMorning : GreetingBase{
+        public override string GetMessage() =>"おはよう";
+    }
+
+    class GreetingAfternoon : GreetingBase {
+        public override string GetMessage() => "こんにちは";
+    }
+
+    class GreetingEvening : GreetingBase {
+        public override string GetMessage() =>"こんばんは";
+    }
+}
