65920a3b4917dcac22deeab464cb3988d0e4101b
郡 元気 <OIBC34018@ojs.ac.jp>
2023-10-18 12:03:20 +0900

    Strategyパターン

---
+++ Chapter17/Strategy/DistanceConverter/ConcreteConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter {
+    // List 17-13 (ConverterBaseの具象クラス)
+
+    public class MeterConverter : ConverterBase {
+        protected override double Ratio { get => 1; }
+        public override string UnitName { get => "メートル"; }
+
+        public override bool IsMyUnit(string name) =>
+            name.ToLower() == "meter" || name == UnitName;
+    }
+
+    public class FeetConverter : ConverterBase {
+        protected override double Ratio { get => 0.3048; }
+        public override string UnitName { get => "フィート"; }
+
+        public override bool IsMyUnit(string name) =>
+            name.ToLower() == "feet" || name == UnitName;
+    }
+
+    public class InchConverter : ConverterBase {
+        protected override double Ratio { get => 0.0254; }
+        public override string UnitName { get => "インチ"; }
+
+        public override bool IsMyUnit(string name) =>
+            name.ToLower() == "inch" || name == UnitName;
+    }
+
+    public class YardConverter : ConverterBase {
+        protected override double Ratio { get => 0.9144; }
+        public override string UnitName { get =>"ヤード"; }
+
+        public override bool IsMyUnit(string name) =>
+            name.ToLower() == "yard" || name == UnitName;
+    }
+}
---
+++ Chapter17/Strategy/DistanceConverter/ConverterFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter {
+    class ConverterFactory {
+        private static ConverterBase[] _conveters = new ConverterBase[] {
+            new MeterConverter(),
+            new FeetConverter(),
+            new YardConverter(),
+            new InchConverter(),
+        };
+
+        public static ConverterBase GetInstance(string name) =>
+            _conveters.FirstOrDefault(x => x.IsMyUnit(name));
+    }
+}
