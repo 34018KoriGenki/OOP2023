7e884bcded5f7d3199d453432b18345e9dcd4756
郡 元気 <OIBC34018@ojs.ac.jp>
2023-10-24 11:55:44 +0900

    WPFのサンプルプログラム

---
+++ Chapter17/演習問題/Exercise03/ToHankakuProcessor.cs
@@ -0,0 +1,30 @@
+﻿using Exercise03.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise03 {
+    class ToHankakuProcessor : ITextFileService {
+
+        private static Dictionary<char, char> _dictionary = new Dictionary<char, char> {
+            { '０','0'},{ '１','1'},{ '２','2'},{ '３','3'},{ '４','4'},
+            { '５','5'},{ '６','6'},{ '７','7'},{ '８','8'},{ '９','9'},
+        };
+
+        private List<string> _newLine = new List<string>();
+
+        public void Initialize(string fname) { }
+
+        void ITextFileService.Execute(string line) {
+            var newLine = Regex.Replace(line, "[０-９]", c => _dictionary[c.Value[0]].ToString());
+            _newLine.Add(newLine);
+        }
+        void ITextFileService.Terminate() {
+            _newLine.ForEach(s => Console.WriteLine(s));
+        }
+
+    }
+}
